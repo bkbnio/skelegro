c412ae1032fee98a1c1ea6115a89acc262bdd008
Ryan Brink <ryanbrink@protonmail.com>
2021-03-14 11:09:28 -0400

    buildsrc

---
+++ buildSrc/src/main/kotlin/org.leafygreens.skelegro.kotlin-common-conventions.gradle.kts
@@ -0,0 +1,103 @@
+group = "org.leafygreens"
+version = run {
+  val baseVersion =
+    project.findProperty("project.version") ?: error("project.version must be set in gradle.properties")
+  when ((project.findProperty("release") as? String)?.toBoolean()) {
+    true -> baseVersion
+    else -> "$baseVersion-SNAPSHOT"
+  }
+}
+
+plugins {
+  id("org.jetbrains.kotlin.jvm")
+  id("io.gitlab.arturbosch.detekt")
+  id("com.github.johnrengelman.shadow")
+  idea
+  `maven-publish`
+}
+
+repositories {
+  jcenter()
+}
+
+dependencies {
+  // Align versions of all Kotlin components
+  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+
+  // TESTING
+  val junitVersion = "5.6.2"
+  val mockkVersion = "1.10.0"
+  val truthVersion = "1.0.1"
+
+  // Use JUnit Jupiter API
+  testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+  // Mockk
+  testImplementation("io.mockk:mockk:$mockkVersion")
+
+  // Truth
+  testImplementation("com.google.truth:truth:$truthVersion")
+}
+
+tasks {
+  test {
+    useJUnitPlatform()
+  }
+  withType<AbstractTestTask> {
+    testLogging {
+      setExceptionFormat("full")
+    }
+    afterSuite(
+      KotlinClosure2({ desc: TestDescriptor, result: TestResult ->
+        if (desc.parent == null) { // will match the outermost suite
+          println(
+            "Results: ${result.resultType} (${result.testCount} tests, " +
+                "${result.successfulTestCount} successes, " +
+                "${result.failedTestCount} failures, ${result.skippedTestCount} skipped)"
+          )
+        }
+      })
+    )
+  }
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+      jvmTarget = "14"
+    }
+  }
+  named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    @Suppress("DEPRECATION")
+    classifier = "shadow"
+  }
+}
+
+detekt {
+  toolVersion = "1.16.0-RC2"
+  config = files("${rootProject.projectDir}/detekt.yml")
+  buildUponDefaultConfig = true
+}
+
+dependencies {
+  detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.16.0-RC2")
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "GithubPackages"
+      url = uri("https://maven.pkg.github.com/lg-backbone/skelegro")
+      credentials {
+        username = System.getenv("GITHUB_ACTOR")
+        password = System.getenv("GITHUB_TOKEN")
+      }
+    }
+  }
+  publications {
+    create<MavenPublication>("library") {
+      from(components["kotlin"])
+      artifact(tasks["shadowJar"])
+    }
+  }
+}
--- gradlegro/build.gradle.kts
+++ gradlegro/build.gradle.kts
@@ -1,83 +1,8 @@
-group = "org.leafygreens"
-version = run {
-  val baseVersion =
-    project.findProperty("project.version") ?: error("project.version must be set in gradle.properties")
-  when ((project.findProperty("release") as? String)?.toBoolean()) {
-    true -> baseVersion
-    else -> "$baseVersion-SNAPSHOT"
-  }
-}
-
 plugins {
-  id("org.jetbrains.kotlin.jvm") version "1.4.20"
-  id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
-  id("com.github.johnrengelman.shadow") version "6.1.0"
-  `java-library`
-  `java-library-distribution`
-  `maven-publish`
-}
-
-repositories {
-  // Use JCenter for resolving dependencies.
-  jcenter()
+  id("org.leafygreens.skelegro.kotlin-common-conventions")
 }
 
 dependencies {
-  // Align versions of all Kotlin components
-  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-  // Use the Kotlin JDK 8 standard library.
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-  // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-  implementation("com.google.guava:guava:29.0-jre")
-
+  // todo can i remove this safely?
   implementation("com.squareup:kotlinpoet:1.7.2")
-
-  val junitVersion = "5.6.2"
-  val truthVersion = "1.0.1"
-
-  // Junit
-  testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
-  testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-
-  // Truth
-  testImplementation("com.google.truth:truth:$truthVersion")
-}
-
-tasks {
-  test {
-    useJUnitPlatform()
-  }
-  build {
-    dependsOn("ktlintFormat")
-  }
-  distributions {
-    main {
-      distributionBaseName.set(rootProject.name)
-    }
-  }
-  named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
-    @Suppress("DEPRECATION")
-    classifier = "shadow"
-  }
-}
-
-publishing {
-  repositories {
-    maven {
-      name = "GithubPackages"
-      url = uri("https://maven.pkg.github.com/rgbrizzlehizzle/proboscidea")
-      credentials {
-        username = System.getenv("GITHUB_ACTOR")
-        password = System.getenv("GITHUB_TOKEN")
-      }
-    }
-  }
-  publications {
-    create<MavenPublication>("runtime") {
-      from(components["kotlin"])
-      artifact(tasks["shadowJar"])
-    }
-  }
 }
