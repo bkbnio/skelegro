a54f3318c24d3d650a8f88442ab26c7cb32c9ad8
Ryan Brink <5607577+rgbrizzlehizzle@users.noreply.github.com>
2021-03-14 11:18:13 -0400

    add detekt step (#1)

--- actiongro/src/main/kotlin/org/leafygreens/skelegro/actiongro/ActionEntityExtensions.kt
+++ actiongro/src/main/kotlin/org/leafygreens/skelegro/actiongro/ActionEntityExtensions.kt
@@ -13,13 +13,17 @@ object ActionEntityExtensions {
     return entity
   }
 
-  fun GithubAction.nestedEntities(init: ActionEntity.NestedEntitiesWrapper.() -> Unit): ActionEntity.NestedEntitiesWrapper {
+  fun GithubAction.nestedEntities(
+    init: ActionEntity.NestedEntitiesWrapper.() -> Unit
+  ): ActionEntity.NestedEntitiesWrapper {
     val entity = ActionEntity.NestedEntitiesWrapper().apply(init)
     entities.add(entity)
     return entity
   }
 
-  fun ActionEntity.NestedEntityWrapper.nestedEntity(init: ActionEntity.NestedEntity.() -> Unit): ActionEntity.NestedEntity {
+  fun ActionEntity.NestedEntityWrapper.nestedEntity(
+    init: ActionEntity.NestedEntity.() -> Unit
+  ): ActionEntity.NestedEntity {
     val entity = ActionEntity.NestedEntity().apply(init)
     value = entity
     return entity
@@ -37,30 +41,35 @@ object ActionEntityExtensions {
     return entity
   }
 
-  fun ActionEntity.NestedEntity.nestedEntities(init: ActionEntity.NestedEntities.() -> Unit): ActionEntity.NestedEntities {
+  fun ActionEntity.NestedEntity.nestedEntities(
+    init: ActionEntity.NestedEntities.() -> Unit
+  ): ActionEntity.NestedEntities {
     val entities = ActionEntity.NestedEntities().apply(init)
     value = entities
     return entities
   }
 
-  fun ActionEntity.NestedEntitiesWrapper.stringEntity(init: ActionEntity.StringEntity.() -> Unit): ActionEntity.StringEntity {
+  fun ActionEntity.NestedEntitiesWrapper.stringEntity(
+    init: ActionEntity.StringEntity.() -> Unit
+  ): ActionEntity.StringEntity {
     val entity = ActionEntity.StringEntity().apply(init)
     values.add(entity)
     return entity
   }
 
-  fun <T> ActionEntity.NestedEntities.arrayEntity(init: ActionEntity.ArrayEntity<T>.() -> Unit): ActionEntity.ArrayEntity<T> {
+  fun <T> ActionEntity.NestedEntities.arrayEntity(
+    init: ActionEntity.ArrayEntity<T>.() -> Unit
+  ): ActionEntity.ArrayEntity<T> {
     val entity = ActionEntity.ArrayEntity<T>().apply(init)
     values.add(entity)
     return entity
   }
 
-  fun <T> ActionEntity.NestedEntity.arrayEntity(init: ActionEntity.ArrayEntity<T>.() -> Unit): ActionEntity.ArrayEntity<T> {
+  fun <T> ActionEntity.NestedEntity.arrayEntity(
+    init: ActionEntity.ArrayEntity<T>.() -> Unit
+  ): ActionEntity.ArrayEntity<T> {
     val entity = ActionEntity.ArrayEntity<T>().apply(init)
     value = entity
     return entity
   }
-
-
-
 }
--- dockergro/src/main/kotlin/org/leafygreens/skelegro/dockergro/commands/Cmd.kt
+++ dockergro/src/main/kotlin/org/leafygreens/skelegro/dockergro/commands/Cmd.kt
@@ -5,11 +5,12 @@ import org.leafygreens.skelegro.dockergro.Dockerfile
 typealias Instructions = List<String>
 
 class Cmd(
-    var instructions: Instructions = emptyList(),
-    comment: String? = null
+  var instructions: Instructions = emptyList(),
+  comment: String? = null
 ) : Command(comment) {
   override fun tag() = "CMD"
-  override fun toString() = "${tag()} ${instructions.joinToString(prefix = "[\"", separator = "\", \"", postfix = "\"]")}"
+  override fun toString() =
+    "${tag()} ${instructions.joinToString(prefix = "[\"", separator = "\", \"", postfix = "\"]")}"
 }
 
 fun Dockerfile.CMD(init: Cmd.() -> Unit): Cmd {
