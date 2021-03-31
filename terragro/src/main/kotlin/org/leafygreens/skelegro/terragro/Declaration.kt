764eb08de42c07af220d13880b521f8b31f03838
Ryan Brink <ryanbrink@protonmail.com>
2021-03-31 16:43:06 -0400

    object shell

--- terragro/src/main/kotlin/org/leafygreens/skelegro/terragro/Declaration.kt
+++ terragro/src/main/kotlin/org/leafygreens/skelegro/terragro/Declaration.kt
@@ -1,6 +1,7 @@
 package org.leafygreens.skelegro.terragro
 
 import org.leafygreens.skelegro.utils.Constants.TAB
+import org.leafygreens.skelegro.utils.Constants.quoted
 import org.leafygreens.skelegro.utils.EntityBuilder
 
 enum class VariableType(val value: String) {
@@ -11,7 +12,7 @@ enum class VariableType(val value: String) {
 
 sealed class Declaration : EntityBuilder {
 
-  class VariableDeclaration(
+  class Variable(
     var name: String? = null,
     var type: VariableType? = null,
     var sensitive: Boolean = false
@@ -23,14 +24,41 @@ sealed class Declaration : EntityBuilder {
       appendLine("}")
     }
   }
-  // resource declaration
+
+  class Resource(
+    var type: String? = null,
+    var name: String? = null,
+    var entities: MutableList<DeclarationEntity> = mutableListOf()
+  ) : Declaration() {
+    override fun toString() = buildEntity {
+      appendLine("resource ${quoted(type!!)} ${quoted(name!!)} {")
+      entities.forEach { entity -> entity.toString().lines().forEach { appendLine("$TAB$it") } }
+      appendLine("}")
+    }
+  }
   // provider declaration
   // terraform declaration
 }
 
 sealed class DeclarationEntity : EntityBuilder {
   // simple (string, int, bool)
+  class Simple<T>(
+    var key: String? = null,
+    var value: T? = null
+  ) : DeclarationEntity() {
+    override fun toString() = when(value!!) {
+      is String -> "$key = ${quoted(value.toString())}"
+      else -> "$key = $value"
+    }
+  }
   // map
   // set
   // object
+  class Object(var name: String? = null, var values: MutableList<DeclarationEntity> = mutableListOf()) : DeclarationEntity() {
+    override fun toString() = buildEntity {
+      appendLine("$name {")
+      // TODO
+      appendLine("}")
+    }
+  }
 }
--- terragro/src/test/kotlin/org/leafygreens/skelegro/terragro/DeclarationTest.kt
+++ terragro/src/test/kotlin/org/leafygreens/skelegro/terragro/DeclarationTest.kt
@@ -8,7 +8,7 @@ internal class DeclarationTest {
   @Test
   fun `Can build variable declaration`() {
     // when
-    val declaration = Declaration.VariableDeclaration("github_token", type = VariableType.STRING, sensitive = true)
+    val declaration = Declaration.Variable("github_token", type = VariableType.STRING, sensitive = true)
 
     // do
     val result = declaration.toString()
@@ -18,6 +18,30 @@ internal class DeclarationTest {
       variable "github_token" {
         type = string
         sensitive = true
+      }
+    """.trimIndent()
+    assertThat(result).isEqualTo(expected)
+  }
+
+  @Test
+  fun `Can build a resource declaration`() {
+    // when
+    val entities = mutableListOf<DeclarationEntity>(
+      DeclarationEntity.Object("metadata"),
+      DeclarationEntity.Object("spec")
+    )
+    val declaration = Declaration.Resource("kubernetes_deployment", "backbone_generator", entities)
+
+    // do
+    val result = declaration.toString()
+
+    // expect
+    val expected = """
+      resource "kubernetes_deployment" "backbone_generator" {
+        metadata {
+        }
+        spec {
+        }
       }
     """.trimIndent()
     assertThat(result).isEqualTo(expected)
