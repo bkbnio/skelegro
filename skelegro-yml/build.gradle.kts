0db2da71cf7d310b2e4b9c961b6082dc13d6d8e7
Ryan Brink <5607577+unredundant@users.noreply.github.com>
2022-01-13 08:53:51 -0500

    chore: bump versions and cleanup (#39)

--- skelegro-yml/build.gradle.kts
+++ skelegro-yml/build.gradle.kts
@@ -1,8 +1,35 @@
 plugins {
-  id("io.bkbn.sourdough.library")
+  kotlin("jvm")
+  id("io.bkbn.sourdough.library.jvm") version "0.5.5"
+  id("io.gitlab.arturbosch.detekt") version "1.19.0"
+  id("com.adarshr.test-logger") version "3.1.0"
+  id("org.jetbrains.dokka")
+  id("maven-publish")
+  id("java-library")
+  id("signing")
 }
 
-dependencies {
-  testImplementation(libs.bundles.test.impl)
-  testImplementation(libs.bundles.test.run)
+sourdough {
+  githubOrg.set("bkbnio")
+  githubRepo.set("skelegro")
+  libraryName.set("Skelegro YML")
+  libraryDescription.set("Kotlin DSL for generating YML Files")
+  licenseName.set("MIT License")
+  licenseUrl.set("https://mit-license.org")
+  developerId.set("unredundant")
+  developerName.set("Ryan Brink")
+  developerEmail.set("admin@bkbn.io")
+}
+
+testing {
+  suites {
+    named("test", JvmTestSuite::class) {
+      useJUnitJupiter()
+      dependencies {
+        implementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+        implementation("io.mockk:mockk:1.10.0")
+        implementation("com.google.truth:truth:1.0.1")
+      }
+    }
+  }
 }
